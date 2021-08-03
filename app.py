0240eb75e6c6c0a64a607a700fcd15bf58679973
madatoo <magda.ruszaj@gmail.com>
2021-08-03 22:30:47 +0000

    added script.js, content to the base.html and temporary data to index.html to check connection between mongo db and heroku

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask,render_template,
+    redirect, request, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
@@ -7,9 +11,21 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+"""
+route for home page
+"""
+
 @app.route("/")
-def hello():
-    return "hello"
+def index():
+    plants = mongo.db.plants.find()
+    return render_template("index.html", page_title="Home", plants=plants)
 
 
 if __name__ == "__main__":
