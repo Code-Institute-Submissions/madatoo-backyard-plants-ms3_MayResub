078e764b10300a64865473d952b72acd5fb7d448
madatoo <magda.ruszaj@gmail.com>
2021-08-04 00:33:29 +0000

    added add plant file, route to this file and custom Materialize select validation

--- app.py
+++ app.py
@@ -29,6 +29,37 @@ def index():
     return render_template("index.html", page_title="Home")
 
 
+"""
+route to add plant page
+"""
+
+
+@app.route("/add_plant", methods=["GET", "POST"])
+def add_plant():
+    if request.method == "POST":
+        wish_list = "on" if request.form.get(
+            "plant_on_wish_list") else "no"
+        plant = {
+            "category_name": request.form.get("category_name"),
+            "plant_name": request.form.get("plant_name"),
+            "plant_img": request.form.get("plant_img"),
+            "plant_on_wish_list": wish_list,
+            "plant_description": request.form.get("plant_description"),
+            "plant_place": request.form.getlist("plant_place"),
+            "plant_tips": request.form.get("plant_tips"),
+            "plant_more_info": request.form.get("plant_more_info"),
+            "plant_notes": request.form.getlist("plant_notes")
+        }
+
+        mongo.db.plants.insert_one(plant)
+        flash("Plant Successfully Added.")
+        return redirect(url_for("index"))
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    places = mongo.db.places.find().sort("plant_places", 1)
+    return render_template(
+        "add_plant.html", categories=categories, places=places)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
